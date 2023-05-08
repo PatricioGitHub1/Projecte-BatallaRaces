3578cd58b6fc8e547f4227312be341804a286be6
Aleex555 <95920070+Aleex555@users.noreply.github.com>
2023-05-08 17:05:32 +0200

    Add files via upload

--- M2/CREATE_DATABASE.sql
+++ M2/CREATE_DATABASE.sql
@@ -1,10 +1,10 @@
-use sys;
+
 drop database if exists RacesPAC;
 create database RacesPAC character set utf8;
 use RacesPAC;
 
 create table if not exists WEAPONS(
-Weapon_id int primary key auto_increment,
+Weapon_id int primary key,
 Weapon_name varchar(30),
 Weapon_Image_Path varchar(100));
 
@@ -14,11 +14,11 @@ Warrior_name varchar(45),
 Warrior_Image_Path varchar (100));
 
 create table if not exists PLAYERS(
-player_id int,
-player_name varchar(45));
+Player_id int primary key auto_increment,
+Player_name varchar(45));
 
 create table if not exists WEAPONS_AVAILABLE(
-Warrior_id int,	
+Warrior_id int not null,	
 Weapon_id int);
 
 create table if not exists BATTLE(
@@ -34,7 +34,7 @@ Battle_Points int);
 
 
 create table if not exists ROUNDS(
-Round_id int primary key auto_increment,
+Round_id int,
 Battle_id int,
 Warrior_id int,
 Warrior_Weapon_id int,
@@ -43,6 +43,49 @@ Opponent_Weapon_id int,
 Injuries_Caused int,
 Injuries_Suffered int,
 Battle_Points int);
+
+
+alter table ROUNDS add constraint PK_ROUNDS primary key (Round_id,Battle_id);
+alter table BATTLE add constraint FK_PLAYER_BATTLE
+	foreign key(Player_id)
+    references PLAYERS(Player_id);
+
+alter table ROUNDS add constraint FK_BATTLE_ROUNDS
+foreign key(Battle_id)
+	references BATTLE(Battle_id);
+    
+    alter table WEAPONS_AVAILABLE add constraint FK_WARRIORS_WEAPONS_AVAILABLE
+foreign key(Warrior_id)
+	references WARRIORS(Warrior_id);
+    
+    
+    alter table WEAPONS_AVAILABLE add constraint FK_WEAPONS_WEAPONS_AVAILABLE
+foreign key(Weapon_id)
+	references WEAPONS(Weapon_id);
+    
+    
+    
+    
+alter table BATTLE add constraint FK_WARRIORS_BATTLE
+foreign key(Warrior_id)
+	references WARRIORS(Warrior_id);
+    
+alter table BATTLE add constraint FK_OPPONENT_BATTLE
+foreign key(Opponent_id)
+	references WARRIORS(Warrior_id);
+    
+    
+    
+    
+alter table BATTLE add constraint FK_OPPONENTW_BATTLE
+foreign key(Opponent_Weapon_id)
+	references WEAPONS(Weapon_id);
+    
+    
+alter table BATTLE add constraint FK_WEAPONS_BATTLE
+foreign key(Warrior_Weapon_id)
+	references WEAPONS(Weapon_id);
+    
 
 
     
